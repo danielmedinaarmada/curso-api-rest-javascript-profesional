9ab3741bdd3c4690fc253dc9d00ead1ef9acff3a
Daniel Medina Armada <daniel@medinaarmada.com>
2024-06-23 13:22:26 -0400

    change fetch by axios

--- src/main.js
+++ src/main.js
@@ -1,15 +1,28 @@
+//vamos a crear la configuracion por defecto que vamos a usa en nuestras consultas
+// no lo podemos agregar header como defecto
+// tambien podemos agregar parametros por defecto
+const api = axios.create({
+  baseURL: "https://api.themoviedb.org/3/",
+  params: {
+    api_key: API_KEY,
+    language: "es",
+  },
+  headers: {
+    "Content-Type": "application/json;utf-8",
+  },
+});
+
 //Peliculas en tendencias
 async function getTrendingMoviesPreview() {
-  const res = await fetch(
-    "https://api.themoviedb.org/3/trending/movie/day?api_key=" + API_KEY + '&language=es'
-  );
-  const data = await res.json();
+  const { data } = await api("/trending/movie/day");  //la respuesta ya viene parseada en JSON y destructuramos status y/o data
   const movies = data.results;
 
   console.log({ data, movies });
 
   movies.forEach((movie) => {
-    const trendingPrevieMovieContainter = document.querySelector("#trendingPreview .trendingPreview-movieList");
+    const trendingPrevieMovieContainter = document.querySelector(
+      "#trendingPreview .trendingPreview-movieList"
+    );
     const movieContainter = document.createElement("div");
     movieContainter.classList.add("movie-container");
     const movieImg = document.createElement("img");
@@ -27,24 +40,23 @@ async function getTrendingMoviesPreview() {
 
 //Lista de cateogorias o genero de peliculas
 async function getCategoriesPreview() {
-  const res = await fetch(
-    "https://api.themoviedb.org/3/genre/movie/list?api_key=" + API_KEY + '&language=es'
-  );
-  const data = await res.json();
+  const { data } = await api("genre/movie/list"); //la respuesta ya viene parseada en JSON y destructuramos status y/o data
   const categories = data.genres;
 
   console.log({ data, categories });
 
   categories.forEach((category) => {
-    const previewCategoriesContainer = document.querySelector("#categoriesPreview .categoriesPreview-list");
+    const previewCategoriesContainer = document.querySelector(
+      "#categoriesPreview .categoriesPreview-list"
+    );
     const categoryContainter = document.createElement("div");
     categoryContainter.classList.add("category-container");
-    
+
     const categoryTitle = document.createElement("h3");
     categoryTitle.classList.add("category-title");
     categoryTitle.setAttribute("id", "id" + category.id);
     const categoryTitleText = document.createTextNode(category.name);
- 
+
     categoryTitle.appendChild(categoryTitleText);
     categoryContainter.appendChild(categoryTitle);
     previewCategoriesContainer.appendChild(categoryContainter);
