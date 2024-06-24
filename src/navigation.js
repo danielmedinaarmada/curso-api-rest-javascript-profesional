1b7056ea0ed87c57b3057841a38854542772bd21
Daniel Medina Armada <daniel@medinaarmada.com>
2024-06-24 12:14:37 -0400

    optimizacion de if

--- src/navigation.js
+++ src/navigation.js
@@ -1,41 +1,39 @@
-window.addEventListener('DOMContentLoaded', navigator, false);
-window.addEventListener('hashchange', navigator, false);
+window.addEventListener("DOMContentLoaded", navigator, false);
+window.addEventListener("hashchange", navigator, false);
 
-function navigator(){
-  console.log({location});
-  
-  if (location.hash.startsWith('#trends')){
-    trendsPages();
-  } else if (location.hash.startsWith('#search=')){
-    searchPages()
-  } else if (location.hash.startsWith('#movie=')){
-    movieDetailsPage();
-  } else if (location.hash.startsWith('#category=')){
-    categoriesPages();
-  } else {
-    homePage();
-  }
+function navigator() {
+  console.log({ location });
+  const pages = [
+    { name: "trends", hashStart: "#trends", render: trendsPages },
+    { name: "search", hashStart: "#search=", render: searchPages },
+    { name: "movie", hashStart: "#movie=", render: movieDetailsPage },
+    { name: "category", hashStart: "#category", render: categoriesPages },
+  ];
+  const hash = windows.location.hash;
+  let redering = homePage;
+  const searchIndexRenderPage = pages.findIndex(page => hash.startsWith(page.hashStart));
+  if (searchIndexRenderPage !== -1 ) redering = pages[searchIndexRenderPage].render;
+  redering();
 }
 
-
-function homePage(){
+function homePage() {
   console.log("HOME!!!");
   getTrendingMoviesPreview();
   getCategoriesPreview();
 }
 
-function categoriesPages(){
+function categoriesPages() {
   console.log("CATEGORIES!!!");
 }
 
-function movieDetailsPage(){
+function movieDetailsPage() {
   console.log("MOVIE!!!");
 }
 
-function searchPages(){
+function searchPages() {
   console.log("SEARCH!!!");
 }
 
-function trendsPages(){
+function trendsPages() {
   console.log("TRENDS!!!");
 }
